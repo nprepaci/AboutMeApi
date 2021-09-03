db91a0ed2844c4d7ea3ed161aebb7ec0c9b2df6f
nickrepaci <nickrepaci@gmail.com>
2021-09-03 00:57:14 -0400

    Added app icon and API unit test

--- AboutMeApiTests/AboutMeApiTests.swift
+++ AboutMeApiTests/AboutMeApiTests.swift
@@ -9,18 +9,44 @@ import XCTest
 @testable import AboutMeApi
 
 class AboutMeApiTests: XCTestCase {
+  
+  //Function that tests the API to ensure it returns data successfully
+  func testAPI() {
+    
+    // Create an expectation for a background download task.
+    let expectation = XCTestExpectation(description: "Test if API is returning data")
+    
+    // Create a URL for a web page to be downloaded.
+    let url = URL(string: "https://api.thecatapi.com/v1/breeds")!
+    
+    // Create a background task to download the web page.
+    let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+      
+      // Make sure we downloaded some data.
+      XCTAssertNotNil(data, "No data was downloaded.")
+      
+      // Fulfill the expectation to indicate that the background task has finished successfully.
+      expectation.fulfill()
+    }
+    
+    // Start the download task.
+    dataTask.resume()
+    
+    // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+    wait(for: [expectation], timeout: 10.0)
+  }
+  
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+  
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
     }
 
     func testPerformanceExample() throws {
--- AboutMeApiUITests/AboutMeApiUITests.swift
+++ AboutMeApiUITests/AboutMeApiUITests.swift
@@ -6,29 +6,42 @@
 //
 
 import XCTest
+import AboutMeApi
 
 class AboutMeApiUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    }
+  
+  
+  // Clicks on a row in the table view
+      func testTableViewCatClick() throws {
+        
         let app = XCUIApplication()
         app.launch()
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Abyssinian"]/*[[".cells.staticTexts[\"Abyssinian\"]",".staticTexts[\"Abyssinian\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.scrollViews.otherElements.staticTexts["5"].swipeUp()
+        
+        imagevir
+    
+  }
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+      func testUserProfileClick() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        app.navigationBars["CatBook"].buttons["Button"].tap()
+        app.scrollViews.otherElements.containing(.image, identifier:"banner").element.swipeUp()
+        
+      
+      
     }
 
     func testLaunchPerformance() throws {
